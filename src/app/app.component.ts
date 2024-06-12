eb1ff92ecdc4163996d02f19de7b9890cdc46184
Miguel Quintana <miguelquintanadev@gmail.com>
2024-06-12 13:58:11 -0400

    INIT

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { InvoiceComponent } from './components/invoice/invoice.component';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [InvoiceComponent, CommonModule, RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = '2-invoice-app';
+}
