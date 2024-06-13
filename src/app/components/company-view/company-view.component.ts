3296631f453d5b3497f20fdd8b6a68203c7c5290
Miguel Quintana <miguelquintanadev@gmail.com>
2024-06-12 20:26:13 -0400

    Components

---
+++ src/app/components/company-view/company-view.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+import { Company } from '../../models/company';
+
+@Component({
+  selector: 'company-view',
+  standalone: true,
+  imports: [],
+  templateUrl: './company-view.component.html'
+})
+export class CompanyViewComponent {
+
+  @Input() company: Company = {name: '', fiscalNumber: 0};
+}
--- src/app/models/company.ts
+++ src/app/models/company.ts
@@ -3,6 +3,5 @@ import { Address } from "./address";
 export class Company {
     name!: string;
     fiscalNumber!: number;
-    address!: Address;
 
 }
