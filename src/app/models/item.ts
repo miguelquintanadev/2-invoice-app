2e0f7b101d9c4590c6e5f17a09e1392b0d2ce061
Miguel Quintana <miguelquintanadev@gmail.com>
2024-06-12 22:39:15 -0400

    Final

--- src/app/models/item.ts
+++ src/app/models/item.ts
@@ -3,6 +3,9 @@ export class Item {
     id!: number;
     product!: string;
     price!: number;
-    quantity!: number;
+    quantity!: number;  
 
+    total(): number {
+        return (this.price * this.quantity);
+    }
 }
